05b42580c5db18cb4d70c8efd0e904dc4a352f5a
Patrick Lewis <4015312+locus313@users.noreply.github.com>
2024-09-02 23:42:55 +0000

    refactor(Script): update github-repo-from-template

--- github-repo-from-template/github-repo-from-template.sh
+++ github-repo-from-template/github-repo-from-template.sh
@@ -11,20 +11,31 @@ TEMPLATE_REPO=${TEMPLATE_REPO:-''}
 REPO_NAME=$1
 CD_USERNAME=${CD_USERNAME:-''}
 CD_GITHUB_TOKEN=${CD_GITHUB_TOKEN:-''}
-PORTX_TENANT_FLUX_ADMINS=${PORTX_TENANT_FLUX_ADMINS:-''}
-PORTX_INFRASTRUCTURE=${PORTX_INFRASTRUCTURE:-''}
+REPO_ADMIN=${REPO_ADMIN:-'admins example'}
+REPO_WRITE=${REPO_WRITE:-'write example2'}
+
+# Check if GITHUB_TOKEN is empty
+if [ -z "${GITHUB_TOKEN}" ]
+then
+      echo "GITHUB_TOKEN is empty. Please set your token and try again"
+      exit 1
+fi
 
 # Create repo from template
-curl -X POST -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.baptiste-preview+json" "${API_URL_PREFIX}/repos/${ORG}/${TEMPLATE_REPO}/generate" -d '{"name":"'${REPO_NAME}'", "owner":"'${ORG}'", "private":true, "include_all_branches":true}';
+curl -s -X POST -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.baptiste-preview+json" "${API_URL_PREFIX}/repos/${ORG}/${TEMPLATE_REPO}/generate" -d '{"name":"'${REPO_NAME}'", "owner":"'${ORG}'", "private":true, "include_all_branches":true}';
 
 # Give internal teams permissions on the new repo
-curl -X PUT -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" "${API_URL_PREFIX}/orgs/${ORG}/teams/${PORTX_TENANT_FLUX_ADMINS}/repos/${ORG}/${REPO_NAME}" -d '{"permission":"admin"}';
-curl -X PUT -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" "${API_URL_PREFIX}/orgs/${ORG}/teams/${PORTX_INFRASTRUCTURE}/repos/${ORG}/${REPO_NAME}" -d '{"permission":"admin"}';
+for ADMIN_TEAM in ${REPO_ADMIN}; do
+    curl -s -X PUT -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" "${API_URL_PREFIX}/orgs/${ORG}/teams/${ADMIN_TEAM}/repos/${ORG}/${REPO_NAME}" -d '{"permission":"admin"}';
+done
 
+for WRITE_TEAM in ${REPO_WRITE}; do
+    curl -s -X PUT -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" "${API_URL_PREFIX}/orgs/${ORG}/teams/${WRITE_TEAM}/repos/${ORG}/${REPO_NAME}" -d '{"permission":"push"}';
+done
 # Give cd user write permissions on the new repo
-curl -X PUT -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" "${API_URL_PREFIX}/repos/${ORG}/${REPO_NAME}/collaborators/${CD_USERNAME}" -d '{"permission":"push"}';
+curl -s -X PUT -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" "${API_URL_PREFIX}/repos/${ORG}/${REPO_NAME}/collaborators/${CD_USERNAME}" -d '{"permission":"push"}';
 
 # Accept the invite automatically 
-CD_INVITES=$(curl -H "Authorization: token ${CD_GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" "${API_URL_PREFIX}/user/repository_invitations")
+CD_INVITES=$(curl -s -H "Authorization: token ${CD_GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" "${API_URL_PREFIX}/user/repository_invitations")
 CD_INVITES_INVITE_ID=$(echo "$CD_INVITES" | jq -r --arg REPO_NAME "${REPO_NAME}" 'select(.[].repository.name==$REPO_NAME) | .[].id')
-curl -X PATCH -H "Authorization: token ${CD_GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" "${API_URL_PREFIX}/user/repository_invitations/${CD_INVITES_INVITE_ID}"
+curl -s -X PATCH -H "Authorization: token ${CD_GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" "${API_URL_PREFIX}/user/repository_invitations/${CD_INVITES_INVITE_ID}"
